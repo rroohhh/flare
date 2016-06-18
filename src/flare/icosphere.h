bf4954fc2ae5023f1797a4db3c2bb32e378643d0
Robin Heinemann <robin.ole.heinemann@t-online.de>
2016-06-18 02:32:04 +0200

    works now

--- src/flare/icosphere.h
+++ src/flare/icosphere.h
@@ -6,15 +6,17 @@
 #include "types.h"
 #include "vector.h"
 #include <algorithm>
+#include <cassert>
+#include <unordered_map>
 
 namespace flr {
     FLARE_CLASS(IcoSphere), public Mesh {
-        using v3 = flr::v3;
+        using v3   = flr::v3;
+        using Edge = flr::Vector2D<flr::u64>;
 
     public:
         IcoSphere(flr::Mesh base, flr::f64 radius, flr::u64 subdiv) {
-            v3       center = {0, 0, 0};
-            flr::u64 this_vertex;
+            v3 center = {0, 0, 0};
 
             for(auto vert : base.points()) { center += vert; }
 
@@ -26,55 +28,117 @@ namespace flr {
 
             std::vector<flr::Face> new_fcs;
             fcs = std::vector<flr::Face>(base.faces());
-            std::vector<std::pair<flr::u64, flr::f64>> angles;
-
-            flr::u64 this_index;
-            flr::u64 next_index;
-            flr::u64 new_index;
+            u64 a, b, c;
 
             for(u64 i = 0; i < subdiv; i++) {
-                new_fcs.clear();
 
                 for(auto face : fcs) {
-                    center = {0, 0, 0};
+                    assert(face.vertices().size() == 3);
 
-                    for(auto vertex : face.vertices()) {
-                        center += verts[vertex];
-                    }
+                    // base_vertex = verts.size() - 1;
 
-                    verts.push_back(center / face.vertices().size());
-                    new_index = verts.size() - 1;
+                    // for(flr::u64 i = 0; i < face.vertices().size(); i++) {
+                    //     // std::cout << "pushing back " << face.vertices()[i
+                    //     %
+                    //     // 3]
+                    //     //           << " and " << face.vertices()[(i + 1) %
+                    //     3]
+                    //     //           << std::endl;
+                    //     verts.push_back((verts[face.vertices()[i % 3]] +
+                    //                      verts[face.vertices()[(i + 1) % 3]])
+                    //                      /
+                    //                     2);
+                    // }
 
-                    angles.clear();
-                    angles.push_back(std::make_pair(face.vertices()[0], 0));
+                    // std::cout << "face: " << face.vertices()[0] << ", "
+                    //           << face.vertices()[1] << ", "
+                    //           << face.vertices()[2] << std::endl;
 
-                    for(u64 i = 1; i < face.vertices().size(); i++) {
-                        this_vertex = face.vertices()[i];
+                    a = middlepoint(face.vertices()[0], face.vertices()[1]);
 
-                        angles.push_back(std::make_pair(
-                            this_vertex, verts[this_vertex].angleBetween(
-                                             verts[face.vertices()[0]])));
-                    }
+                    b = middlepoint(face.vertices()[1], face.vertices()[2]);
 
-                    std::sort(angles.begin(), angles.end(), [](auto a, auto b) {
-                        return a.second < b.second;
-                    });
+                    c = middlepoint(face.vertices()[2], face.vertices()[0]);
 
-                    angles.push_back(std::make_pair(face.vertices()[0], 0));
+                    new_fcs.push_back({{face.vertices()[0], a, c}});
+                    new_fcs.push_back({{face.vertices()[1], b, a}});
+                    new_fcs.push_back({{face.vertices()[2], c, b}});
+                    new_fcs.push_back({{a, b, c}});
 
-                    for(flr::u64 i = 0; i < angles.size() - 1; i++) {
-                        this_index = angles[i].first;
-                        next_index = angles[i + 1].first;
-                        new_fcs.push_back(std::vector<u64>(
-                            {new_index, this_index, next_index}));
-                    }
+                    // for(flr::u64 i = 0; i < face.vertices().size(); i++) {
+                    //     // std::cout << "pushing back " << i << " and "
+                    //     //           << base_vertex + i + 1 << " and "
+                    //     //           << base_vertex + ((i + 2) % 3) + 1 <<
+                    //     //           std::endl;
+
+                    //     new_fcs.push_back(
+                    //         {{face.vertices()[i],
+                    //           middlepoint(face.vertices()[i],
+                    //                       face.vertices()[(i + 1) % 3]),
+                    //           middlepoint(face.vertices()[(i + 2) % 3],
+                    //                       face.vertices()[(i + 3) % 3])}});
+                    // }
+
+                    // new_fcs.push_back(
+                    //     {{base_vertex + 1, base_vertex + 2, base_vertex +
+                    //     3}});
                 }
 
                 fcs = std::vector<flr::Face>(new_fcs);
+                new_fcs.clear();
 
-                for(auto & vert : verts) { vert /= vert.norm(); }
+                // for(auto & vert : verts) { vert /= vert.norm() / radius; }
             }
         }
+
+        u64 middlepoint(u64 a, u64 b) {
+            u64 smallerIndex = a < b ? a : b;
+            u64 greaterIndex = a < b ? b : a;
+            u64 key          = (smallerIndex << 32) + greaterIndex;
+            // std::cout << "key for middle of " << smallerIndex << " and "
+            //           << greaterIndex << " is " << key << std::endl;
+            auto p = points.find(key);
+
+            if(p != points.end()) {
+                // std::cout << "key existed: " << p->second << std::endl;
+                return p->second;
+            }
+
+            verts.push_back((verts[a] + verts[b]) / 2);
+
+            points.insert(std::make_pair(key, verts.size() - 1));
+
+            return verts.size() - 1;
+        }
+
+        std::vector<Edge> facesToEdges(std::vector<flr::Face> faces) {
+            std::vector<Edge> edges;
+
+            for(auto face : faces) {
+                for(u64 i = 0; i < face.vertices().size(); i++) {
+                    edges.push_back(
+                        {face.vertices()[i],
+                         face.vertices()[(i + 1) % face.vertices().size()]});
+                }
+            }
+
+            std::sort(edges.begin(), edges.end(), [](Edge a, Edge b) {
+                if(a.x != b.x)
+                    return a.x > b.x;
+                else if(a.y != b.y)
+                    return a.y > b.y;
+                else
+                    return true;
+            });
+
+            auto last = std::unique(edges.begin(), edges.end());
+            edges.erase(last, edges.end());
+
+            return edges;
+        }
+
+    private:
+        std::unordered_map<u64, u64> points;
     };
 }
 
